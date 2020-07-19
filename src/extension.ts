29dbe9a162df17ed39071f50eec8e62c6e39c419
Yong Wang <yongwang@uark.edu>
2020-07-19 15:24:45 -0500

    implement using ts/js and cURL

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,56 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function modify_bibkey(bibtex:string) {
+	if (bibtex.indexOf('DOI Not Found') >= 0) return bibtex;
+	const chars_to_ignore = '!@#$%^&*()=+,./<>?~\[\]\\\{\}|;:"';
+	const chars_to_space = '-_';
+	const words_to_ignore = ['this ','that ','and ','or ','a ','an ','the ','of ',
+                        'as ','on ','via ','by ','in ','at ',
+						"'s ","' "];
+	const lines = bibtex.split('\n');
+	let ss = lines[0].split('{');
+	let title = '';
+	let bibkey = '';
+    // get the orginal bibkey: which has the format of Author_Year
+    bibkey = ss[ss.length-1].replace('_','').replace(',','')
+	// get the title line
+	lines.forEach(line => {
+		if (line.trim().startsWith('title')) title = line.trim();
+	});   
+	ss = title.split('=');
+	title = ss[ss.length-1].toLowerCase();
+	console.log('orignal title = ' + title);
+	var i;
+	for(i=0; i<chars_to_ignore.length; i++) {
+		title = title.split(chars_to_ignore.charAt(i)).join('').trim();
+	}
+	for(i=0; i<chars_to_space.length; i++) {
+		title = title.split(chars_to_space.charAt(i)).join(' ').trim();
+	}
+	for(i=0; i<words_to_ignore.length; i++) {
+		title = title.split(words_to_ignore[i]).join('').trim();
+	}
+	console.log('clean title = ' + title);
+    // get the title words
+	let words = title.split(' ');
+	for(i=0; i<words.length; i++) {
+		words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+	}
+    // generate the new bibkey
+	var n = 3;
+	if(words.length<3) n = words.length;
+	for(i=0; i<n; i++) {
+		bibkey = bibkey + words[i];
+	}
+	// generate new bibkey
+	let newbibtex = lines[0].split('{')[0]+'{'+bibkey+',\n';
+	for(i=1; i<lines.length; i++) {
+		newbibtex = newbibtex + lines[i] + '\n';
+	}
+	return newbibtex;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -22,35 +72,29 @@ export function activate(context: vscode.ExtensionContext) {
 			doi = doi.trim().replace(' ','');
 			console.log('doi = '+doi);
 			// vscode.window.showInformationMessage('Getting bibtex for '+doi);
-			// run python code to get the bibtex entry (string)
-			const cmd = 'python get_bibtex_from_doi.py ' + doi + '> tmp_bibtex.txt';
+			const cmd = 'curl -L -H "Accept: application/x-bibtex" https://doi.org/' + doi;
 			console.log('cmd = '+cmd);
             const cp = require('child_process');
-			cp.execSync(cmd, {timeout:5000}); // wait for the command to finish
-			// get the bibtex string from saved file
-            var fs = require('fs');
-            fs.readFile('tmp_bibtex.txt', 'utf-8', function (err: any, data: any) {
-                if (err) throw err;
+			cp.exec(cmd, { timeout: 5000 }, (err: any, data: string, stderr: string) => {
                 editor.edit(editBuilder => {
-					console.log('data = ');
-					console.log(data);
-					if (data.length > 5) {
-						editBuilder.replace(new vscode.Range(editor.selection.active.line, 0, editor.selection.active.line+1, 0), data);
-						if (data.indexOf('DOI Not Found')>=0) { // did not found the DOI, show error
-							vscode.window.showErrorMessage('DOI Not Found: '+doi)
-							console.log('Error: DOI Not Found');
-						}
-						else { // sucess
-							vscode.window.showInformationMessage('Success for bibtex from '+doi);
-							console.log('Success!');
-						}
-					}
-					else { // get nothing
-						vscode.window.showErrorMessage('No response from doi.org for '+doi);
-						console.log('Error: data is too short, likely get nothing from doi.org.');
-					}
+                    if (data.length > 5) {
+						data = modify_bibkey(data);
+                        editBuilder.replace(new vscode.Range(editor.selection.active.line, 0, editor.selection.active.line+1, 0), data);
+                        if (data.indexOf('DOI Not Found')>=0) { // did not found the DOI, show error
+                            vscode.window.showWarningMessage('DOI Not Found: '+doi)
+                            console.log('Error: DOI Not Found');
+                        }
+                        else { // sucess
+                            vscode.window.showInformationMessage('Success for bibtex from '+doi);
+                            console.log('Success!');
+                        }
+                    };
                 });
-			});
+                if (err) {
+                    vscode.window.showErrorMessage('Error in DOI2Bib ...');
+                    console.log('error: ' + err);
+                };
+            }); // wait for the command to finish
         };
 	});
 	context.subscriptions.push(disposable);
